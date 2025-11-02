e50f25e77743f59d8c9acd2d75d7846276d1485c
G-PaperChain <g.paperchain@gmail.com>
2025-11-02 16:55:19 +0300

    .

--- backend/clients/urls.py
+++ backend/clients/urls.py
@@ -4,5 +4,5 @@ from .views import create_client, get_client_list
 
 urlpatterns = [
     path("create-client/", create_client, name="create-client"),
-    path("clients/", get_client_list, name="client-list"),
+    path("clients/", get_client_list, name="client-list")
 ]
