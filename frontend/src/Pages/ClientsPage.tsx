8ea6e7538f3ee0aed93d01fab111ed171de7ebdf
G-PaperChain <g.paperchain@gmail.com>
2026-03-05 23:56:57 +0300

    ref: Clients Page refactor

--- frontend/src/Pages/ClientsPage.tsx
+++ frontend/src/Pages/ClientsPage.tsx
@@ -1,32 +1,28 @@
-import React, { useState, useContext } from 'react'
-import MobileHeader from '../layout/MobileHeader.jsx'
-import ClientsHeader from '../features/clients/components/ClientsHeader.jsx'
-import ClientsDataTable from '@/features/clients/components/ClientsDataTable.js'
+import React, { useState, useContext } from "react";
+import MobileHeader from "../layout/MobileHeader.jsx";
+import ClientsHeader from "../features/clients/components/ClientsHeader.js";
+import ClientsDataTable from "@/features/clients/components/ClientsDataTable.js";
 
 const Clients = () => {
-
    return (
       <div className="flex flex-col lg:flex-row bg-white text-black dark:bg-[#000000] dark:text-white transition-colors duration-300">
-
          <header>
             <MobileHeader />
          </header>
 
-         <div className="invisible pl-[296px] hidden lg:sticky lg:top-0 lg:bottom-0 lg:left-0 lg:block">
-         </div>
+         <div className="invisible pl-[296px] hidden lg:sticky lg:top-0 lg:bottom-0 lg:left-0 lg:block"></div>
 
-         <main className='min-w-0 flex-1 pt-8 pb-12' >
-            <div className="flex flex-col gap-8" >
+         <main className="min-w-0 flex-1 pt-8 pb-12">
+            <div className="flex flex-col gap-8">
                <ClientsHeader />
 
                <div className="px-4 lg:px-8">
                   <ClientsDataTable />
                </div>
-
             </div>
          </main>
       </div>
-   )
-}
+   );
+};
 
-export default Clients
+export default Clients;
