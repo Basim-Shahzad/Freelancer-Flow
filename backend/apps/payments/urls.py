e74188768422efef603e6d1f110e34f561dfb150
G-PaperChain <g.paperchain@gmail.com>
2026-03-05 22:39:52 +0300

    feat: Remove payments app and all related files, including models, views, serializers, and migrations

--- backend/apps/payments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('get-revenue/', views.get_revenue , name='get_revenue'),
-    path('invoices/', views.InvoiceListCreateView.as_view(), name='invoice_list_create'),
-    path('invoices/<int:invoice_id>/', views.InvoiceDetailView.as_view(), name='invoice_detail'),
-]
