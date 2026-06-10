1fdc4edf459a4426d9fd4745a494c32f47156492
G-PaperChain <g.paperchain@gmail.com>
2026-06-10 17:30:54 +0300

    Remove old Django backend and React frontend from repo (keep locally)

--- backend/apps/projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-   path('projects/', views.ProjectsListCreateApiView.as_view(), name='projects_list_create'),
-   path('projects/<uuid:id>/', views.ProjectRetrieveUpdateDestroyApiView.as_view(), name='projects_detail'),
-]
